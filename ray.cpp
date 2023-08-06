7ef177b6d60ca3dd4842deaedff57e2916d4a5a3
Stone Chen <chen.stonechen@gmail.com>
2023-08-06 11:25:33 -0400

    very basic pixel loop

--- ray.cpp
+++ ray.cpp
@@ -21,7 +21,9 @@ int main(int ArgCount, char **Args)
     SDL_Window   *window   = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, SDL_WINDOW_POPUP_MENU);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_Texture  *texture  = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, Width, Height);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    u32 *Pixels = (u32 *) malloc(Width * Height * sizeof(u32));
+
 
     while(is_running == true)
     {
@@ -46,7 +48,24 @@ int main(int ArgCount, char **Args)
         }
 
 
+        u32 *PixelPointer = Pixels;
+        for(u32 Row = 0; Row < Height; ++Row)
+        {
+            for(u32 Col = 0; Col < Width; ++Col)
+            {
+                *PixelPointer++ = 0xFF33FF00;
+            }
+        }
+
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
+        SDL_UpdateTexture(texture, NULL, Pixels, (int)(Width * sizeof(u32)));
+        SDL_RenderCopyEx(renderer, texture, NULL, NULL, 0, NULL, SDL_FLIP_VERTICAL);
+        SDL_RenderPresent(renderer);
+
+
+
         SDL_RenderPresent(renderer);
 
     }
